cc3d926d602a724f9ba9428d9e7c442df04c0e25
Pierre-Reis <pierre.developer@hotmail.com>
2022-10-08 16:20:47 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,14 +1,14 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,23 +23,18 @@ import lombok.ToString;
 @EqualsAndHashCode
 
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable{
-	
+@Table(name = "tb_order")
+public class Order implements Serializable{
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String name;
+	private Instant moment;
 	
-	private String email;
-	
-	private String phone;
-	
-	private String password;
-	
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
 }
